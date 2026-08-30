bbce5dd3fd6fe147fbece456619875d9b150a01b
juanrod <juanrod@Skyss-MacBook-Neo.local>
2026-08-29 19:14:46 -0500

    Revert "Merge pull request #19 from juanroddotdev/feature/onboarding-current-location"
    This reverts commit db4accbc815c0e19afd46c66a9fa286e1985b8e0, reversing
    changes made to ef3cefd0d7a0a080bcc398bcc89e1fc0e3c58fef.

--- js/storage.js
+++ js/storage.js
@@ -224,8 +224,7 @@ export async function applyFirstRunDefaults() {
   }
 
   const userTz = Intl.DateTimeFormat().resolvedOptions().timeZone;
-  const coords = await getCurrentPosition();
-  const hubIds = getOnboardingPinIds(userTz, coords);
+  const hubIds = getOnboardingPinIds(userTz);
   const pins = hubIds.map((id, index) => pinEntryFromId(id, Date.now() + index));
 
   await writeStorage({
@@ -234,27 +233,4 @@ export async function applyFirstRunDefaults() {
   });
 
   return pins;
-}
-
-function getCurrentPosition(timeoutMs = 2500) {
-  return new Promise((resolve) => {
-    if (!navigator.geolocation) {
-      resolve(null);
-      return;
-    }
-
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        resolve({
-          lat: position.coords.latitude,
-          lon: position.coords.longitude,
-        });
-      },
-      () => resolve(null),
-      {
-        timeout: timeoutMs,
-        maximumAge: 600_000,
-      },
-    );
-  });
 }
